8040e189671ab1ae20e9820c380d61304031fa08
Bice93 <bianca.cerulo@hotmail.it>
2022-06-28 14:48:49 +0200

    Milestone 2

--- js/main.js
+++ js/main.js
@@ -7,51 +7,51 @@
 // MILESTONE 1:
 // Stampare su console le informazioni di nome, ruolo e la stringa della foto
 
-// Wayne Barnett	Founder & CEO	        wayne-barnett-founder-ceo.jpg
-// Angela Caroll	Chief Editor	        angela-caroll-chief-editor.jpg
-// Walter Gordon	Office Manager	        walter-gordon-office-manager.jpg
-// Angela Lopez	    Social Media Manager	angela-lopez-social-media-manager.jpg
-// Scott Estrada	Developer	            scott-estrada-developer.jpg
-// Barbara Ramos	Graphic Designer	    barbara-ramos-graphic-designer.jpg
-
-
-
 const teamMembers = [
     {
-        nome : ` Wayne Barnett`,
-        ruolo : `Founder & CEO`,
-        foto : `wayne-barnett-founder-ceo.jpg`
+        nome: ` Wayne Barnett`,
+        ruolo: `Founder & CEO`,
+        foto: `wayne-barnett-founder-ceo.jpg`
     },
     {
-        nome : ` Angela Caroll`,
-        ruolo : `Chief Editor`,
-        foto : `angela-caroll-chief-editor.jpg`
+        nome: ` Angela Caroll`,
+        ruolo: `Chief Editor`,
+        foto: `angela-caroll-chief-editor.jpg`
     },
     {
-        nome : ` Walter Gordon`,
-        ruolo : `Office Manager`,
-        foto : `walter-gordon-office-manager.jpg`
+        nome: ` Walter Gordon`,
+        ruolo: `Office Manager`,
+        foto: `walter-gordon-office-manager.jpg`
     },
     {
-        nome : ` Angela Lopez`,
-        ruolo : `Social Media Manager`,
-        foto : `angela-lopez-social-media-manager.jpg`
+        nome: ` Angela Lopez`,
+        ruolo: `Social Media Manager`,
+        foto: `angela-lopez-social-media-manager.jpg`
     },
     {
-        nome : ` Scott Estrada`,
-        ruolo : `Developer`,
-        foto : `scott-estrada-developer.jpg`
+        nome: ` Scott Estrada`,
+        ruolo: `Developer`,
+        foto: `scott-estrada-developer.jpg`
     },
     {
-        nome : ` Barbara Ramos`,
-        ruolo : `Graphic Designer`,
-        foto : `barbara-ramos-graphic-designer.jpg`
+        nome: ` Barbara Ramos`,
+        ruolo: `Graphic Designer`,
+        foto: `barbara-ramos-graphic-designer.jpg`
     },
 ];
 console.log(teamMembers)
 
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+let boxMembers = document.getElementById(`container_team`);
+
 //Stampo in console il nome il ruolo e la foto di tutti i membri del team
-for (let i = 0; i < teamMembers.length; i++){
+for (let i = 0; i < teamMembers.length; i++) {
+    
+    boxMembers.innerHTML += `
+    ${teamMembers[i].nome} - ${teamMembers[i].ruolo} - ${teamMembers[i].foto} `;
     console.log(`${teamMembers[i].nome} - ${teamMembers[i].ruolo} - ${teamMembers[i].foto} `);
 };
+
 
