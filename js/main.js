6fd50eb1625335843e675239c8ec4d218e3fc00d
Bice93 <bianca.cerulo@hotmail.it>
2022-06-28 15:44:31 +0200

    Bonus 1 Add Image

--- js/main.js
+++ js/main.js
@@ -6,52 +6,57 @@
 // Chiudete almeno una commit per milestone.
 // MILESTONE 1:
 // Stampare su console le informazioni di nome, ruolo e la stringa della foto
-
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+// BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva
 const teamMembers = [
     {
         nome: ` Wayne Barnett`,
         ruolo: `Founder & CEO`,
-        foto: `wayne-barnett-founder-ceo.jpg`
+        foto: `./img/wayne-barnett-founder-ceo.jpg`,
     },
     {
         nome: ` Angela Caroll`,
         ruolo: `Chief Editor`,
-        foto: `angela-caroll-chief-editor.jpg`
+        foto: `./img/angela-caroll-chief-editor.jpg`
     },
     {
         nome: ` Walter Gordon`,
         ruolo: `Office Manager`,
-        foto: `walter-gordon-office-manager.jpg`
+        foto: `./img/walter-gordon-office-manager.jpg`
     },
     {
         nome: ` Angela Lopez`,
         ruolo: `Social Media Manager`,
-        foto: `angela-lopez-social-media-manager.jpg`
+        foto: `./img/angela-lopez-social-media-manager.jpg`
     },
     {
         nome: ` Scott Estrada`,
         ruolo: `Developer`,
-        foto: `scott-estrada-developer.jpg`
+        foto: `./img/scott-estrada-developer.jpg`
     },
     {
         nome: ` Barbara Ramos`,
         ruolo: `Graphic Designer`,
-        foto: `barbara-ramos-graphic-designer.jpg`
+        foto: `./img/barbara-ramos-graphic-designer.jpg`
     },
 ];
 console.log(teamMembers)
-
-// MILESTONE 2:
-// Stampare le stesse informazioni su DOM sottoforma di stringhe
 
 let boxMembers = document.getElementById(`container_team`);
 
 //Stampo in console il nome il ruolo e la foto di tutti i membri del team
 for (let i = 0; i < teamMembers.length; i++) {
-    
     boxMembers.innerHTML += `
-    ${teamMembers[i].nome} - ${teamMembers[i].ruolo} - ${teamMembers[i].foto} `;
-    console.log(`${teamMembers[i].nome} - ${teamMembers[i].ruolo} - ${teamMembers[i].foto} `);
+    ${teamMembers[i].nome} - ${teamMembers[i].ruolo}`;
+    //console.log(teamMembers[i].foto)
+    const imageMember = document.createElement(`img`);    //BONUS 1
+    imageMember.setAttribute(`src`, teamMembers[i].foto);
+    imageMember.classList.add(`ms_image`)
+    boxMembers.append(imageMember);
+
+    //console.log(`${teamMembers[i].nome} - ${teamMembers[i].ruolo} - ${teamMembers[i].foto} `);
 };
 
 
